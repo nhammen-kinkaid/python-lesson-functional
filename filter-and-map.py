78c711059cb035a2aa853341fed8a8a499e77b10
nhammen-kinkaid <nathaniel.hammen@kinkaid.org>
2026-06-24 20:03:52 +0000

    un-lazy the iterables

--- filter-and-map.py
+++ filter-and-map.py
@@ -5,6 +5,6 @@ def square(x):
   return x*x
 
 my_list = [1, 2, 3.1, "4", "five", 6, 20]
-only_ints = filter(is_int, my_list)
+only_ints = list(filter(is_int, my_list))
 print(only_ints)
-print(map(square, only_ints))
+print(list(map(square, only_ints)))
