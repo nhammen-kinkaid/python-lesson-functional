dc7160f3fa3f78f4926b3610ae96a4d49838bb9a
nhammen-kinkaid <nathaniel.hammen@kinkaid.org>
2026-06-24 12:57:54 -0500

    Create lambda-expressions.py

---
+++ lambda-expressions.py
@@ -0,0 +1,5 @@
+my_list = [1, 2, 3.1, "4", "five", 6, 20]
+only_ints = filter(lambda x: type(x)==int, my_list)
+print(only_ints)
+print(map(lambda z: z*z, only_ints))
+print(reduce(lambda x,y: x+y, map(lambda z: z*z, only_ints)))
